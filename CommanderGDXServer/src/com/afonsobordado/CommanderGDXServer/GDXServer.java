e765e412d8bf4e85e022192740496cae27951cef
Afonso Bordado <afonsobordado@gmail.com>
2015-04-03 21:35:10 +0100

    Fix GDXServer warnings

--- CommanderGDXServer/src/com/afonsobordado/CommanderGDXServer/GDXServer.java
+++ CommanderGDXServer/src/com/afonsobordado/CommanderGDXServer/GDXServer.java
@@ -17,7 +17,6 @@ import com.afonsobordado.CommanderGDXServer.LocalObjects.LocalServerPlayer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
@@ -52,7 +51,7 @@ public class GDXServer {
 	        FixtureDef fixtureDef = new FixtureDef();
 	        fixtureDef.shape = shape;
 	        fixtureDef.density = 1f;
-	        Fixture fixture = body.createFixture(fixtureDef);
+	        body.createFixture(fixtureDef);
 	        shape.dispose();
 	        
 		}
